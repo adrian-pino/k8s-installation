c78b905ca21fec9e5e5c62a89d119d81022bafbf
Adrián Pino <adrian.pino@i2cat.net>
2024-07-02 18:20:59 +0200

    Add cilium installer. Update calico installer.

---
+++ cni-installation/cilium/install-cilium.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# VARIABLES
+################################
+MASTER_NODE_IP=X.Y.Z.T
+POD_CIDR=A.B.C.D
+################################
+
+helm repo add cilium https://helm.cilium.io/
+helm install cilium cilium/cilium \
+    --namespace kube-system \
+    --set ipam.mode=cluster-pool \
+    --set ipam.operator.clusterPoolIPv4PodCIDRList=$POD_CIDR \
+    --set k8sServiceHost=$MASTER_NODE_IP \
+    --set k8sServicePort=6443
+

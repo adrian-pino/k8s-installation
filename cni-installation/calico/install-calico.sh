6c03676b520fffa87b1a13e2a304f4d7d0100416
Adrian Pino <48448195+adrian-pino@users.noreply.github.com>
2024-11-08 14:00:26 +0100

    Update install-calico.sh to v3.29.0

--- cni-installation/calico/install-calico.sh
+++ cni-installation/calico/install-calico.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Install Calico with Kubernetes API datastore, 50 nodes or less:
 # ref: https://docs.tigera.io/calico/latest/getting-started/kubernetes/self-managed-onprem/onpremises
-curl https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/calico.yaml -O
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.29.0/manifests/calico.yaml -O
 kubectl apply -f calico.yaml
 

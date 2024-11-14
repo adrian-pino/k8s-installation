da817941e07d8bddb16b722319d89065919a333a
Adrian Pino <48448195+adrian-pino@users.noreply.github.com>
2024-11-14 09:23:01 +0100

    Add replicas var to Cilium installer

--- cni-installation/cilium/install-cilium.sh
+++ cni-installation/cilium/install-cilium.sh
@@ -4,6 +4,7 @@
 ################################
 MASTER_NODE_IP=X.Y.Z.T
 POD_CIDR=A.B.C.D/E
+REPLICAS=2
 ################################
 
 helm repo add cilium https://helm.cilium.io/
@@ -12,5 +13,5 @@ helm install cilium cilium/cilium \
     --set ipam.mode=cluster-pool \
     --set ipam.operator.clusterPoolIPv4PodCIDRList=$POD_CIDR \
     --set k8sServiceHost=$MASTER_NODE_IP \
-    --set k8sServicePort=6443
-
+    --set k8sServicePort=6443 \
+    --set operator.replicas=$REPLICAS
